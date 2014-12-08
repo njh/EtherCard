e6944f83558bdcb566417acb707b655d4ad289a4
Nicholas Humfrey <njh@aelius.com>
2014-12-08 19:05:19 +0000

    Whitespace and code style consistency fixes using the astyle tool

--- examples/noipClient/noipClient.ino
+++ examples/noipClient/noipClient.ino
@@ -1,7 +1,7 @@
-// NoIP client sketch for ENC28J60 based Ethernet Shield. 
+// NoIP client sketch for ENC28J60 based Ethernet Shield.
 // You need a free account from www.no-ip.com
 //
-// 1. On NoIP website, create a host of type DNS Host (A) 
+// 1. On NoIP website, create a host of type DNS Host (A)
 // 2. Insert your host name in noIP_host[] variable
 // 3. Encode "username:password" in base64 using an online tool like
 // 4. Paste the encoded string in noIP_auth[] variable
@@ -50,222 +50,230 @@ Stash stash;
 
 void setup () {
 
-  Serial.begin(57600);
-  Serial.println(F("NoIP Client Demo"));
-  Serial.println();
+    Serial.begin(57600);
+    Serial.println(F("NoIP Client Demo"));
+    Serial.println();
 
-  if (!ether.begin(sizeof Ethernet::buffer, mymac, 10))
-    Serial.println(F( "Failed to access Ethernet controller"));
-  else
-    Serial.println(F("Ethernet controller initialized"));
-  Serial.println();
+    if (!ether.begin(sizeof Ethernet::buffer, mymac, 10))
+        Serial.println(F( "Failed to access Ethernet controller"));
+    else
+        Serial.println(F("Ethernet controller initialized"));
+    Serial.println();
 
-  if (!ether.dhcpSetup())
-    Serial.println(F("Failed to get configuration from DHCP"));
-  else
-    Serial.println(F("DHCP configuration done"));
+    if (!ether.dhcpSetup())
+        Serial.println(F("Failed to get configuration from DHCP"));
+    else
+        Serial.println(F("DHCP configuration done"));
 
-  ether.printIp("IP Address:\t", ether.myip);
-  ether.printIp("Netmask:\t", ether.netmask);
-  ether.printIp("Gateway:\t", ether.gwip);
-  Serial.println();
+    ether.printIp("IP Address:\t", ether.myip);
+    ether.printIp("Netmask:\t", ether.netmask);
+    ether.printIp("Gateway:\t", ether.gwip);
+    Serial.println();
 
-  actual_status = STATUS_IDLE;
-  attempt = 0;
+    actual_status = STATUS_IDLE;
+    attempt = 0;
 
-  // Resolve IP for getIP_web and noIP_web 
-  // and store them into global variables
-  if (!ether.dnsLookup(getIP_web)) {
-    Serial.print(F("Unable to resolve IP for "));
-    SerialPrint_P(getIP_web);
-    actual_status = STATUS_ERROR;
-  } else {
-    ether.copyIp(getIP_address, ether.hisip);
-    SerialPrint_P(getIP_web);
-    ether.printIp(" resolved to:\t", ether.hisip);
-  }
-  if (!ether.dnsLookup(noIP_web)) {
-    Serial.print(F("Unable to resolve IP for "));
-    SerialPrint_P(noIP_web);
-    actual_status = STATUS_ERROR;
-  } else {
-    ether.copyIp(noIP_address, ether.hisip);
-    SerialPrint_P(noIP_web);
-    ether.printIp(" resolved to:\t", ether.hisip);    
-  }
+    // Resolve IP for getIP_web and noIP_web
+    // and store them into global variables
+    if (!ether.dnsLookup(getIP_web)) {
+        Serial.print(F("Unable to resolve IP for "));
+        SerialPrint_P(getIP_web);
+        actual_status = STATUS_ERROR;
+    } else {
+        ether.copyIp(getIP_address, ether.hisip);
+        SerialPrint_P(getIP_web);
+        ether.printIp(" resolved to:\t", ether.hisip);
+    }
+    if (!ether.dnsLookup(noIP_web)) {
+        Serial.print(F("Unable to resolve IP for "));
+        SerialPrint_P(noIP_web);
+        actual_status = STATUS_ERROR;
+    } else {
+        ether.copyIp(noIP_address, ether.hisip);
+        SerialPrint_P(noIP_web);
+        ether.printIp(" resolved to:\t", ether.hisip);
+    }
 
-  Serial.println();
+    Serial.println();
 }
 
 
 void loop() {
 
-  ether.packetLoop(ether.packetReceive());
+    ether.packetLoop(ether.packetReceive());
 
-  // FSM
-  switch(actual_status) {
+    // FSM
+    switch(actual_status) {
 
-  case STATUS_IDLE: checkPublicIP(); break;
-  case STATUS_WAITING_FOR_PUBLIC_IP: checkPublicIPResponse(); break;
-  case STATUS_NOIP_NEEDS_UPDATE: updateNoIP(); break;
-  case STATUS_WAITING_FOR_NOIP: checkNoIPResponse(); break;
-  }
+    case STATUS_IDLE:
+        checkPublicIP();
+        break;
+    case STATUS_WAITING_FOR_PUBLIC_IP:
+        checkPublicIPResponse();
+        break;
+    case STATUS_NOIP_NEEDS_UPDATE:
+        updateNoIP();
+        break;
+    case STATUS_WAITING_FOR_NOIP:
+        checkNoIPResponse();
+        break;
+    }
 }
 
 void checkPublicIP() {
 
-  if(millis() > check_ip_timer) {
+    if(millis() > check_ip_timer) {
 
-    Serial.print(F("Checking public IP... "));
+        Serial.print(F("Checking public IP... "));
 
-	// Create a request for GetIP service,
-	// set destination IP and send request saving session_id
-    Stash::prepare(PSTR("GET /demo/getip.php HTTP/1.1" "\r\n" "Host: $F" "\r\n" "\r\n"), getIP_web);
-    ether.copyIp(ether.hisip, getIP_address);
-    session_id = ether.tcpSend();
-	
-	// Change FSM state, prepare for timeout and increment attempts counter
-    actual_status = STATUS_WAITING_FOR_PUBLIC_IP;
-    request_timer = millis() + REQUEST_TIMEOUT;
-    attempt++;
-  }
+        // Create a request for GetIP service,
+        // set destination IP and send request saving session_id
+        Stash::prepare(PSTR("GET /demo/getip.php HTTP/1.1" "\r\n" "Host: $F" "\r\n" "\r\n"), getIP_web);
+        ether.copyIp(ether.hisip, getIP_address);
+        session_id = ether.tcpSend();
+
+        // Change FSM state, prepare for timeout and increment attempts counter
+        actual_status = STATUS_WAITING_FOR_PUBLIC_IP;
+        request_timer = millis() + REQUEST_TIMEOUT;
+        attempt++;
+    }
 }
 
 void checkPublicIPResponse() {
 
-  String actualIp, dnsIp;
+    String actualIp, dnsIp;
 
-  const char* reply = ether.tcpReply(session_id);
+    const char* reply = ether.tcpReply(session_id);
 
-  // We got a valid response
-  if(reply != 0) {
+    // We got a valid response
+    if(reply != 0) {
 
-	// Parse response for public IP
-    for(int i = 0; i < strlen(reply) - 189; i++) actualIp = actualIp + reply[187 + i];
-    Serial.println(actualIp);
+        // Parse response for public IP
+        for(int i = 0; i < strlen(reply) - 189; i++) actualIp = actualIp + reply[187 + i];
+        Serial.println(actualIp);
 
-	// If we can't resolve actual IP for our hostname on NoIP,
-	// return to IDLE state and wait for the next interval
-    if(!ether.dnsLookup(noIP_host)) {
-      Serial.print(F("Unable to resolve actual IP for "));
-      SerialPrint_P(noIP_host);
-      Serial.println();
-      actual_status = STATUS_IDLE;
-      attempt = 0;
-      check_ip_timer = millis() + CHECK_IP_INTERVAL;
-    
-	// If we can resolve the IP for our hostname, save it in xxx.xxx.xxx.xxx form
-	// and compare it with our public IP
-	} else {
-      for(int i = 0; i < 4; i++) {
-        dnsIp = dnsIp + String(ether.hisip[i]);
-        if(i < 3) dnsIp = dnsIp + ".";
-      }
-      SerialPrint_P(noIP_host);
-      Serial.print(F(" resolved to "));
-      Serial.println(dnsIp);
-      
-	  // If they are the same, we can sit down and wait for the next interval
-      if(actualIp.compareTo(dnsIp) == 0) {
-        Serial.println(F("No update needed :)"));
-        actual_status = STATUS_IDLE;
-        attempt = 0;
-        check_ip_timer = millis() + CHECK_IP_INTERVAL;        
-      
-	  // Different? We'd better to update NoIP!
-	  } else {
-        Serial.println(F("Update needed :("));
-        actual_status = STATUS_NOIP_NEEDS_UPDATE;
-        attempt = 0;
-      }
-    }    
-  
-  // No valid response? Check for timeout
-  // If we've already sent a max number of requests, return to IDLE state
-  // and wait for the next interval
-  } else {
-    if(millis() > request_timer) {
-      Serial.println(F(" no response :("));    
-      actual_status = STATUS_IDLE;
-      if(attempt == MAX_ATTEMPTS) {
-        Serial.println(F("Max number of attempts reached"));
-        attempt = 0;
-        check_ip_timer = millis() + CHECK_IP_INTERVAL;
-      }
+        // If we can't resolve actual IP for our hostname on NoIP,
+        // return to IDLE state and wait for the next interval
+        if(!ether.dnsLookup(noIP_host)) {
+            Serial.print(F("Unable to resolve actual IP for "));
+            SerialPrint_P(noIP_host);
+            Serial.println();
+            actual_status = STATUS_IDLE;
+            attempt = 0;
+            check_ip_timer = millis() + CHECK_IP_INTERVAL;
+
+            // If we can resolve the IP for our hostname, save it in xxx.xxx.xxx.xxx form
+            // and compare it with our public IP
+        } else {
+            for(int i = 0; i < 4; i++) {
+                dnsIp = dnsIp + String(ether.hisip[i]);
+                if(i < 3) dnsIp = dnsIp + ".";
+            }
+            SerialPrint_P(noIP_host);
+            Serial.print(F(" resolved to "));
+            Serial.println(dnsIp);
+
+            // If they are the same, we can sit down and wait for the next interval
+            if(actualIp.compareTo(dnsIp) == 0) {
+                Serial.println(F("No update needed :)"));
+                actual_status = STATUS_IDLE;
+                attempt = 0;
+                check_ip_timer = millis() + CHECK_IP_INTERVAL;
+
+                // Different? We'd better to update NoIP!
+            } else {
+                Serial.println(F("Update needed :("));
+                actual_status = STATUS_NOIP_NEEDS_UPDATE;
+                attempt = 0;
+            }
+        }
+
+        // No valid response? Check for timeout
+        // If we've already sent a max number of requests, return to IDLE state
+        // and wait for the next interval
+    } else {
+        if(millis() > request_timer) {
+            Serial.println(F(" no response :("));
+            actual_status = STATUS_IDLE;
+            if(attempt == MAX_ATTEMPTS) {
+                Serial.println(F("Max number of attempts reached"));
+                attempt = 0;
+                check_ip_timer = millis() + CHECK_IP_INTERVAL;
+            }
+        }
     }
-  }
 }
 
 void updateNoIP() {
 
-  Serial.print(F("Updating NoIP..."));
+    Serial.print(F("Updating NoIP..."));
 
-  // Create a request for updating NoIP using NoIP API,
-  // set destination IP and send request saving session_id
-  Stash::prepare(PSTR("GET /nic/update?hostname=$F HTTP/1.0" "\r\n"
-    "Host: $F" "\r\n"
-    "Authorization: Basic $F" "\r\n"
-    "User-Agent: NoIP_Client" "\r\n" "\r\n"),
-  noIP_host, noIP_web, noIP_auth);
-  ether.copyIp(ether.hisip, noIP_address);
-  session_id = ether.tcpSend();
-  
-  // Wait for response or timeout...
-  actual_status = STATUS_WAITING_FOR_NOIP;
-  request_timer = millis() + REQUEST_TIMEOUT;
-  attempt++;
+    // Create a request for updating NoIP using NoIP API,
+    // set destination IP and send request saving session_id
+    Stash::prepare(PSTR("GET /nic/update?hostname=$F HTTP/1.0" "\r\n"
+                        "Host: $F" "\r\n"
+                        "Authorization: Basic $F" "\r\n"
+                        "User-Agent: NoIP_Client" "\r\n" "\r\n"),
+                   noIP_host, noIP_web, noIP_auth);
+    ether.copyIp(ether.hisip, noIP_address);
+    session_id = ether.tcpSend();
+
+    // Wait for response or timeout...
+    actual_status = STATUS_WAITING_FOR_NOIP;
+    request_timer = millis() + REQUEST_TIMEOUT;
+    attempt++;
 }
 
 void checkNoIPResponse() {
 
-  const char* reply = ether.tcpReply(session_id);
-  boolean done;
+    const char* reply = ether.tcpReply(session_id);
+    boolean done;
 
-  // We got a valid response...
-  if(reply != 0) {
+    // We got a valid response...
+    if(reply != 0) {
 
-	// Parse NoIP response looking for status/error codes...
-    if(strstr(reply, "good") != 0) {
-      Serial.println(F(" done!"));
-      done = true;
-    } 
-    else if(strstr(reply, "nochg") != 0) {
-      Serial.println(F(" no change required!"));
-      done = true;
-    }
-    else if(strstr(reply, "nohost") != 0) Serial.println(F(" host not found :("));
-    else if(strstr(reply, "badauth") != 0) Serial.println(F(" invalid username or password :("));
-    else if(strstr(reply, "badagent") != 0) Serial.println(F(" agent banned :("));
-    else if(strstr(reply, "!donator") != 0) Serial.println(F(" feature not available for specified username :("));
-    else if(strstr(reply, "abuse") != 0) Serial.println(F(" username blocked due to abuse :("));
-    else Serial.println(F(" generic error :("));
-    
-	// Record has been updated? Ok wait for next interval...
-    if(done) {   
-      actual_status = STATUS_IDLE;
-      attempt = 0;
-      check_ip_timer = millis() + CHECK_IP_INTERVAL;
-    }
-  
-  // No valid response? Check for timeout
-  // If we've already sent a max number of requests, return to IDLE state
-  // and wait for the next interval
-  } else {
+        // Parse NoIP response looking for status/error codes...
+        if(strstr(reply, "good") != 0) {
+            Serial.println(F(" done!"));
+            done = true;
+        }
+        else if(strstr(reply, "nochg") != 0) {
+            Serial.println(F(" no change required!"));
+            done = true;
+        }
+        else if(strstr(reply, "nohost") != 0) Serial.println(F(" host not found :("));
+        else if(strstr(reply, "badauth") != 0) Serial.println(F(" invalid username or password :("));
+        else if(strstr(reply, "badagent") != 0) Serial.println(F(" agent banned :("));
+        else if(strstr(reply, "!donator") != 0) Serial.println(F(" feature not available for specified username :("));
+        else if(strstr(reply, "abuse") != 0) Serial.println(F(" username blocked due to abuse :("));
+        else Serial.println(F(" generic error :("));
 
-    if(millis() > request_timer) {
-      Serial.println(F("No response from NoIP"));
-      if(attempt == MAX_ATTEMPTS) {
-        Serial.println(F("Max number of attempts reached"));
-        actual_status = STATUS_IDLE;
-        attempt = 0;
-        check_ip_timer = millis() + CHECK_IP_INTERVAL;
-      } 
-      else
-        actual_status = STATUS_NOIP_NEEDS_UPDATE;
+        // Record has been updated? Ok wait for next interval...
+        if(done) {
+            actual_status = STATUS_IDLE;
+            attempt = 0;
+            check_ip_timer = millis() + CHECK_IP_INTERVAL;
+        }
+
+        // No valid response? Check for timeout
+        // If we've already sent a max number of requests, return to IDLE state
+        // and wait for the next interval
+    } else {
+
+        if(millis() > request_timer) {
+            Serial.println(F("No response from NoIP"));
+            if(attempt == MAX_ATTEMPTS) {
+                Serial.println(F("Max number of attempts reached"));
+                actual_status = STATUS_IDLE;
+                attempt = 0;
+                check_ip_timer = millis() + CHECK_IP_INTERVAL;
+            }
+            else
+                actual_status = STATUS_NOIP_NEEDS_UPDATE;
+        }
     }
-  }
 }
 
 void SerialPrint_P(PGM_P str) {
-  for (uint8_t c; (c = pgm_read_byte(str)); str++) Serial.write(c);
+    for (uint8_t c; (c = pgm_read_byte(str)); str++) Serial.write(c);
 }
--- webutil.cpp
+++ webutil.cpp
@@ -46,24 +46,24 @@ uint8_t EtherCard::findKeyVal (const char *str,char *strbuf, uint8_t maxlen,cons
     uint8_t i=0;
     const char *kp;
     kp=key;
-    while(*str &&  *str!=' ' && *str!='\n' && found==0){
-        if (*str == *kp){
+    while(*str &&  *str!=' ' && *str!='\n' && found==0) {
+        if (*str == *kp) {
             kp++;
-            if (*kp == '\0'){
+            if (*kp == '\0') {
                 str++;
                 kp=key;
-                if (*str == '='){
+                if (*str == '=') {
                     found=1;
                 }
             }
-        }else{
+        } else {
             kp=key;
         }
         str++;
     }
-    if (found==1){
+    if (found==1) {
         // copy the value to a buffer and terminate it with '\0'
-        while(*str &&  *str!=' ' && *str!='\n' && *str!='&' && i<maxlen-1){
+        while(*str &&  *str!=' ' && *str!='\n' && *str!='&' && i<maxlen-1) {
             *strbuf=*str;
             i++;
             str++;
@@ -78,13 +78,13 @@ uint8_t EtherCard::findKeyVal (const char *str,char *strbuf, uint8_t maxlen,cons
 // convert a single hex digit character to its integer value
 unsigned char h2int(char c)
 {
-    if (c >= '0' && c <='9'){
+    if (c >= '0' && c <='9') {
         return((unsigned char)c - '0');
     }
-    if (c >= 'a' && c <='f'){
+    if (c >= 'a' && c <='f') {
         return((unsigned char)c - 'a' + 10);
     }
-    if (c >= 'A' && c <='F'){
+    if (c >= 'A' && c <='F') {
         return((unsigned char)c - 'A' + 10);
     }
     return(0);
@@ -112,12 +112,12 @@ void EtherCard::urlDecode (char *urlbuf)
 void int2h(char c, char *hstr)
 {
     hstr[1]=(c & 0xf)+'0';
-    if ((c & 0xf) >9){
+    if ((c & 0xf) >9) {
         hstr[1]=(c & 0xf) - 10 + 'a';
     }
     c=(c>>4)&0xf;
     hstr[0]=c+'0';
-    if (c > 9){
+    if (c > 9) {
         hstr[0]=c - 10 + 'a';
     }
     hstr[2]='\0';
@@ -129,8 +129,8 @@ void EtherCard::urlEncode (char *str,char *urlbuf)
 {
     char c;
     while ((c = *str) != 0) {
-        if (c == ' '||isalnum(c)){
-            if (c == ' '){
+        if (c == ' '||isalnum(c)) {
+            if (c == ' ') {
                 c = '+';
             }
             *urlbuf=c;
@@ -154,17 +154,17 @@ uint8_t EtherCard::parseIp (uint8_t *bytestr,char *str)
     char *sptr;
     uint8_t i=0;
     sptr=NULL;
-    while(i<4){
+    while(i<4) {
         bytestr[i]=0;
         i++;
     }
     i=0;
-    while(*str && i<4){
+    while(*str && i<4) {
         // if a number then start
-        if (sptr==NULL && isdigit(*str)){
+        if (sptr==NULL && isdigit(*str)) {
             sptr=str;
         }
-        if (*str == '.'){
+        if (*str == '.') {
             *str ='\0';
             bytestr[i]=(atoi(sptr)&0xff);
             i++;
@@ -173,7 +173,7 @@ uint8_t EtherCard::parseIp (uint8_t *bytestr,char *str)
         str++;
     }
     *str ='\0';
-    if (i==3){
+    if (i==3) {
         bytestr[i]=(atoi(sptr)&0xff);
         return(0);
     }
@@ -185,10 +185,12 @@ void EtherCard::makeNetStr (char *resultstr,uint8_t *bytestr,uint8_t len,char se
 {
     uint8_t i=0;
     uint8_t j=0;
-    while(i<len){
+    while(i<len) {
         itoa((int)bytestr[i],&resultstr[j],base);
         // search end of str:
-        while(resultstr[j]){j++;}
+        while(resultstr[j]) {
+            j++;
+        }
         resultstr[j]=separator;
         j++;
         i++;
