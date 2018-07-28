b5a13d6be3e34ea9de80c8124c6fa704dc0019c5
Nicholas Humfrey <njh@aelius.com>
2018-07-28 19:53:52 +0100

    Removed setting MACON1_TXPAUS and MACON1_RXPAUS Change suggested by @mindstormsking in #134: The bits MACON1_TXPAUS and MACON1_RXPAUS are used only in full-duplex mode, while according to all other settings, half-duplex mode is used.

--- enc28j60.cpp
+++ enc28j60.cpp
@@ -388,7 +388,7 @@ byte ENC28J60::initialize (uint16_t size, const byte* macaddr, byte csPin) {
     writeRegByte(ERXFCON, ERXFCON_UCEN|ERXFCON_CRCEN|ERXFCON_PMEN|ERXFCON_BCEN);
     writeReg(EPMM0, 0x303f);
     writeReg(EPMCS, 0xf7f9);
-    writeRegByte(MACON1, MACON1_MARXEN|MACON1_TXPAUS|MACON1_RXPAUS);
+    writeRegByte(MACON1, MACON1_MARXEN);
     writeOp(ENC28J60_BIT_FIELD_SET, MACON3,
             MACON3_PADCFG0|MACON3_TXCRCEN|MACON3_FRMLNEN);
     writeReg(MAIPG, 0x0C12);
