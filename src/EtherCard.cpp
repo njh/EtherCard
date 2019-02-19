ca1fe5d24f3a39d3d097941fe2d425a4f5f3458b
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2019-02-19 23:15:33 +0000

    Fixed

--- src/EtherCard.cpp
+++ src/EtherCard.cpp
@@ -30,7 +30,7 @@ uint16_t EtherCard::delaycnt = 0; //request gateway ARP lookup
 
 uint8_t EtherCard::begin (const uint16_t size,
                           const uint8_t* macaddr,
-                          uint8_t csPin, uint16_t timeout = 60000) {
+                          uint8_t csPin, uint16_t timeout) {
     using_dhcp = false;
 #if ETHERCARD_STASH
     Stash::initMap();
